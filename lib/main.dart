1a8c65e82b1d0dc233bf7afa24355c62ec0ad7e2
skr03 <sudarshan.krishnakumar@sky.uk>
2020-11-28 00:18:21 +0000

    extract screen buttons into separate widgts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:chatapps/screens/login.dart';
+import 'screens/signup.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder> {
+        '/signup': (BuildContext context) => new SignupPage()
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -48,140 +54,96 @@ class _MyHomePageState extends State<MyHomePage> {
         children: <Widget>[
           Container(
               child: Stack(children: <Widget>[
-            Container(
-              padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-              child: Text(
-                'Hello',
-                style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(15.0, 185.0, 0.0, 0.0),
-              child: Text(
-                'Foody',
-                style: TextStyle(fontSize: 70.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(207.0, 175.0, 0.0, 0.0),
-              child: Text(
-                '.',
-                style: TextStyle(
-                    fontSize: 80.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue),
-              ),
-            )
+            HelloWidget(),
+            FoodyWidget(),
+            DotWidget()
           ])),
           Container(
               padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
               child: Column(
                 children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                        labelText: 'EMAIL',
-                        labelStyle: TextStyle(
-                            fontFamily: 'Monteserrat',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue))),
-                  ),
+                  EmailWidget(),
                   SizedBox(height: 20.0),
-                  TextField(
-                    decoration: InputDecoration(
-                        labelText: 'PASSWORD',
-                        labelStyle: TextStyle(
-                            fontFamily: 'Monteserrat',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue))),
-                    obscureText: true,
-                  ),
+                  PasswordWidget(),
                   SizedBox(height: 20.0),
-                  Container(
-                      alignment: Alignment(1.0, 0.0),
-                      padding: EdgeInsets.only(top: 15.0, left: 20.0),
-                      child: InkWell(
-                        child: Text('Forgot Password',
-                            style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Monteserrat',
-                                decoration: TextDecoration.underline)),
-                      )),
+                  ForgotPasswordWidget(),
                   SizedBox(height: 40.0),
-                  Container(
-                    height: 40.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.blueAccent,
-                      color: Colors.blue,
-                      elevation: 7.0,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Center(
-                          child: Text(
-                            'LOGIN',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Monteserrat'),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                  LoginButtonWidget(),
                   SizedBox(height: 20.0),
-                  Container(
-                    height: 40.0,
-                    color: Colors.transparent,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.red,
-                              style: BorderStyle.solid,
-                              width: 1.0),
-                          color: Colors.redAccent,
-                          borderRadius: BorderRadius.circular(20.0)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          SizedBox(height: 10.0),
-                          Center(
-                              child: Text('Login with Google',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      fontFamily: 'Monteserrat')))
-                        ],
-                      ),
-                    ),
-                  )
+                  GoogleLoginWidget()
                 ],
               )),
           SizedBox(height: 15.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'New to Foody ?',
-                style: TextStyle(fontFamily: 'Monteserrat'),
-              ),
-              SizedBox(width: 5.0),
-              InkWell(
-                  onTap: () {},
-                  child: Text('Register',
-                      style: TextStyle(
-                          color: Colors.blue,
-                          fontFamily: 'Monteserrat',
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline)))
-            ],
-          )
+          RegisterWidget()
         ],
       ),
     );
   }
 }
+
+class RegisterWidget extends StatelessWidget {
+  const RegisterWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'New to Foody ?',
+          style: TextStyle(fontFamily: 'Monteserrat'),
+        ),
+        SizedBox(width: 5.0),
+        InkWell(
+            onTap: () {
+              Navigator.of(context).pushNamed('/signup');
+            },
+            child: Text('Register',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontFamily: 'Monteserrat',
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline)))
+      ],
+    );
+  }
+}
+
+class GoogleLoginWidget extends StatelessWidget {
+  const GoogleLoginWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40.0,
+      color: Colors.transparent,
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(
+                color: Colors.red,
+                style: BorderStyle.solid,
+                width: 1.0),
+            color: Colors.redAccent,
+            borderRadius: BorderRadius.circular(20.0)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 10.0),
+            Center(
+                child: Text('Login with Google',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: 'Monteserrat')))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
