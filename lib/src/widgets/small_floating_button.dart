f79fbf8b81bb748863fbcbb89bcc9a2c4254fba1
skr03 <sudarshan.krishnakumar@sky.uk>
2020-12-03 21:59:57 +0000

    added bottom menu bar and popular food section

---
+++ lib/src/widgets/small_floating_button.dart
@@ -0,0 +1,29 @@
+import 'package:chatapps/src/widgets/commons.dart';
+import 'package:flutter/material.dart';
+
+class SmallButton extends StatelessWidget {
+  final IconData icon;
+
+  SmallButton(this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: RED,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Icon(
+            icon,
+            size: 16,
+            color: WHITE,
+          ),
+        ),
+      ),
+    );
+  }
+}
