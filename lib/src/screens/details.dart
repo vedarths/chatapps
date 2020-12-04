b16bdc56507ac35bf7d8117547ac0f426ce4d25f
skr03 <sudarshan.krishnakumar@sky.uk>
2020-12-04 23:10:26 +0000

    add details screen with product name and price

--- lib/src/screens/details.dart
+++ lib/src/screens/details.dart
@@ -1,6 +1,8 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:chatapps/src/models/products.dart';
 import 'package:chatapps/src/helpers/commons.dart';
+import 'package:chatapps/src/widgets/small_floating_button.dart';
+import 'package:chatapps/src/widgets/title.dart';
 import 'package:flutter/material.dart';
 
 class Details extends StatefulWidget {
@@ -39,15 +41,87 @@ class _DetailsState extends State<Details> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        IconButton(icon: Icon(Icons.arrow_back_ios, color: Colors.black,), onPressed: () {Navigator.pop(context);}),
+                        IconButton(
+                            icon: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.black,
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            }),
                         Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Image.asset('images/shopping-bag.png', height: 30, width: 30,),
+                          padding: const EdgeInsets.only(right:8.0),
+                          child: Stack(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Stack(
+                                  children: <Widget>[
+                                    Image.asset(
+                                      'images/shopping-bag.png',
+                                      height: 30,
+                                      width: 30,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Positioned(
+                                right: 3,
+                                bottom: 1,
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                        color: WHITE,
+                                        borderRadius: BorderRadius.circular(10),
+                                        boxShadow: [
+                                          BoxShadow(
+                                              color: GREY,
+                                              offset: Offset(2, 3),
+                                              blurRadius: 3
+                                          )
+                                        ]),
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(left:4, right: 4),
+                                      child: CustomText(
+                                        text: '25',
+                                        color: RED,
+                                        size: 12,
+                                        weight: FontWeight.bold,
+                                      ),
+                                    )),
+                              )
+                            ],
+                          ),
                         )
                       ],
-                    )
+                    ),
+                    Positioned(
+                      right: 14,
+                      bottom: 50,
+                      child: Container(
+                          decoration: BoxDecoration(
+                              color: WHITE,
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: GREY,
+                                    offset: Offset(2, 3),
+                                    blurRadius: 5
+                                )
+                              ]),
+                          child: Padding(
+                            padding: const EdgeInsets.all(6),
+                            child: Icon(
+                              Icons.favorite,
+                              size: 22,
+                              color: RED,
+                            ),
+                          ))
+                    ),
                   ],
-                )),
+                )
+            ),
+            CustomText(text: widget.product.name, size: 26, weight: FontWeight.bold,),
+            CustomText(text: "\$" + widget.product.price.toString(), size: 20, weight: FontWeight.w400,color: RED,)
           ],
         ),
       ),
