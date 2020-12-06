3b91150db6af5b2828998471de1d5f88ca5913c3
skr03 <sudarshan.krishnakumar@sky.uk>
2020-12-06 11:37:31 +0000

    add top navigation bar in cart scren

--- lib/src/screens/bag.dart
+++ lib/src/screens/bag.dart
@@ -1,3 +1,5 @@
+import 'package:chatapps/src/helpers/commons.dart';
+import 'package:chatapps/src/widgets/title.dart';
 import 'package:flutter/material.dart';
 
 class ShoppingBag extends StatefulWidget {
@@ -8,7 +10,53 @@ class ShoppingBag extends StatefulWidget {
 class _ShoppingBagState extends State<ShoppingBag> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: BLACK),
+        backgroundColor: WHITE,
+        elevation: 0,
+        centerTitle: true,
+        title: CustomText(text: 'Shopping Bag'),
+        actions: <Widget> [
+          Padding(
+            padding: const EdgeInsets.only(top:10.0),
+            child: Stack(
+            children: <Widget> [
+               Padding(
+                 padding: const EdgeInsets.all(12.0),
+                 child: Image.asset('images/shopping-bag.png', width: 20, height: 20,),
+               ),
+              Positioned(
+                right: 7,
+                bottom: 5,
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: WHITE,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              color: GREY[400],
+                              offset: Offset(2, 3),
+                              blurRadius: 3
+                          )
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left:4, right: 4),
+                      child: CustomText(
+                        text: '25',
+                        color: RED,
+                        size: 12,
+                        weight: FontWeight.bold,
+                      ),
+                    )),
+              )
+            ]
+            ),
+          )
+        ],
+      ),
+      backgroundColor: WHITE,
+    );
   }
 }
 
